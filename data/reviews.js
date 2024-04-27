740b3407e982c696f3d001dd6e651cc281cee7cc
Sophia Otis <sophianotis3@gmail.com>
2024-04-27 16:48:15 -0600

    test

---
+++ data/reviews.js
@@ -0,0 +1,23 @@
+export const reviews = [
+  {
+    "name": "Jane Doe",
+    "stars": 4,
+    "text": "Great service! The staff was very knowledgeable and friendly. My car was fixed quickly and at a reasonable price. Highly recommend!"
+  },
+  {
+    "name": "Will Jones",
+    "stars": 3,
+    "text": "Had an emergency repair needed and they were able to fit me in right away. Really saved the day!"
+  },
+  {
+    "name": "Tabitha Clark",
+    "stars": 5,
+    "text": "Took my car in for an oil change and they finished it in no time. The waiting area was clean and comfortable, and the staff kept me updated throughout the process."
+  },
+  {
+    "name": "Mark Romano",
+    "stars": 4,
+    "text": "Not only did they fix the issue with my car, but they also gave me some great tips on maintenance to prevent future problems. Excellent customer service!"
+  },
+  
+]
---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+// inject current year in footer
+const numberOne = new Date();
+document.querySelector("#year").textContent = numberOne.getFullYear();
+
+// toggle button switch from X to =
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+// grab reviews data from the file
+import { reviews } from "../data/reviews.js";
+console.log(reviews);
+
+// create an empty card
+const myTarget = document.querySelector("#rcards");
+
+for (let x = 0; x < reviews.length; x++) {
+
+  //create a section
+  const mySection = document.createElement('section')
+  const myName = document.createElement('h3')
+  myName.textContent = reviews[x].name
+  const myReview = document.createElement('p')
+  myReview.textContent = reviews[x].text
+
+  const myRating = document.createElement('div')
+
+  //stars amount
+  
+  for (let step = 0; step<reviews[x].stars; step++) {
+    const revIndicator = document.createElement('img')
+    revIndicator.src = "../images/star.svg"
+    myRating.appendChild(revIndicator)
+  }
+  
+  //build the figure with child elements
+  mySection.appendChild(myName);
+  mySection.appendChild(myRating);
+  mySection.appendChild(myReview);
+
+  //add a new card to a page
+  myTarget.appendChild(mySection);
+
+} // end for loop
